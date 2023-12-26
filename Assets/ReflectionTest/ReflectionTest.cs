55eab0a2b1567cc895a25a8e36674957c50fd856
sapphire-al2o3 <t_telkun@hotmail.com>
2023-12-26 19:21:17 +0900

    new()制約の計測追加

--- Assets/ReflectionTest/ReflectionTest.cs
+++ Assets/ReflectionTest/ReflectionTest.cs
@@ -5,6 +5,36 @@ using UnityEngine.Profiling;
 
 public class ReflectionTest : MonoBehaviour
 {
+    class TestClass
+    {
+        int a;
+    }
+
+    struct TestStruct
+    {
+        int b;
+    }
+
+    T TestCreateInstance<T>()
+    {
+        return System.Activator.CreateInstance<T>();
+    }
+
+    T TestCreateInstance2<T>() where T : new()
+    {
+        return new T();
+    }
+
+    T TestCreateInstance3<T>() where T : struct
+    {
+        return default;
+    }
+
+    TestClass TestCreateInstance4()
+    {
+        return new TestClass();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +59,69 @@ public class ReflectionTest : MonoBehaviour
             }
             Profiler.EndSample();
             Debug.Log(g);
+        }
+
+        {
+            var t = TestCreateInstance<TestClass>();
+            var s = TestCreateInstance<TestStruct>();
+        }
+
+        // 20byte * 1000
+        {
+            Profiler.BeginSample("CreateInstance class");
+            for (int i = 0; i < 1000; i++)
+            {
+                var t = TestCreateInstance<TestClass>();
+            }
+            Profiler.EndSample();
+        }
+
+        // 20byte * 1000
+        {
+            Profiler.BeginSample("CreateInstance struct");
+            for (int i = 0; i < 1000; i++)
+            {
+                var t = TestCreateInstance<TestStruct>();
+            }
+            Profiler.EndSample();
+        }
+
+        // 0byte
+        {
+            Profiler.BeginSample("generic new class");
+            for (int i = 0; i < 1000; i++)
+            {
+                var t = TestCreateInstance2<TestClass>();
+            }
+            Profiler.EndSample();
+        }
+
+        // 0byte
+        {
+            Profiler.BeginSample("generic new struct");
+            for (int i = 0; i < 1000; i++)
+            {
+                var t = TestCreateInstance2<TestStruct>();
+            }
+            Profiler.EndSample();
+        }
+
+        {
+            Profiler.BeginSample("new class");
+            for (int i = 0; i < 1000; i++)
+            {
+                var t = TestCreateInstance4();
+            }
+            Profiler.EndSample();
+        }
+
+        {
+            Profiler.BeginSample("generic default struct");
+            for (int i = 0; i < 1000; i++)
+            {
+                var t = TestCreateInstance3<TestStruct>();
+            }
+            Profiler.EndSample();
         }
     }
 
