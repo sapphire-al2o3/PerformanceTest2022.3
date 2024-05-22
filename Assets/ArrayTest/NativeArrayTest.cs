bc0f10d05e7a7d7caf9c244e279b94f86fbefcd6
sapphire-al2o3 <t_telkun@hotmail.com>
2024-05-22 23:03:50 +0900

    Spanでのアクセス速度計測

--- Assets/ArrayTest/NativeArrayTest.cs
+++ Assets/ArrayTest/NativeArrayTest.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 
 
 public class NativeArrayTest : MonoBehaviour
 {
     void Start()
     {
-        var nativeArray = new NativeArray<byte>(1000, Allocator.Temp);
+        const int Max = 100000;
+        var nativeArray = new NativeArray<byte>(Max, Allocator.Temp);
 
         // 初回呼び出しを計測に含めない
-        nativeArray[0] = 0;
-        var t = nativeArray[0];
+        {
+            int l = nativeArray.Length;
+            nativeArray[0] = 0;
+            var t = nativeArray[0];
+            var rs = nativeArray.AsReadOnlySpan();
+            t = rs[0];
+            var s = nativeArray.AsSpan();
+            s[0] = 0;
+        }
 
         using (new ProfilerScope("NativeArray set"))
         {
@@ -30,9 +39,27 @@ public class NativeArrayTest : MonoBehaviour
             }
         }
 
+        using (new ProfilerScope("NativeArray span get"))
+        {
+            var span = nativeArray.AsReadOnlySpan();
+            for (int i = 0; i < span.Length; i++)
+            {
+                var v = span[i];
+            }
+        }
+
+        using (new ProfilerScope("NativeArray span set"))
+        {
+            var span = nativeArray.AsSpan();
+            for (int i = 0; i < span.Length; i++)
+            {
+                span[i] = 0;
+            }
+        }
+
         nativeArray.Dispose();
 
-        var array = new byte[1000];
+        var array = new byte[Max];
         using (new ProfilerScope("Array set"))
         {
             for (int i = 0; i < array.Length; i++)
